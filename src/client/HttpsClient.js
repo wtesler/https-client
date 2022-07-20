89dd892426d0a05a58691a0a48b2dd715fc3a365
Will Tesler <willtesler@gmail.com>
2022-07-20 11:50:54 -0500

    Include status code with json response

--- src/client/HttpsClient.js
+++ src/client/HttpsClient.js
@@ -3,47 +3,48 @@ module.exports = class HttpsClient {
     const keys = Object.keys(body);
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
+      const value = body[key];
 
       if (i === 0) {
-        path += "?";
+        path += '?';
       }
 
-      path += `${key}=${body[key]}`;
+      path += `${key}=${value}`;
 
       if (i < keys.length - 1) {
-        path += "&";
+        path += '&';
       }
     }
 
-    return this._call("GET", path, host, body, headers);
+    return this._call('GET', path, host, body, headers);
   }
 
   async post(path, host, body = {}, headers = {}) {
     body = JSON.stringify(body);
-    headers["Content-Length"] = body.length;
+    headers['Content-Length'] = body.length;
 
-    return this._call("POST", path, host, body, headers);
+    return this._call('POST', path, host, body, headers);
   }
 
   async put(path, host, body = {}, headers = {}) {
     body = JSON.stringify(body);
-    headers["Content-Length"] = body.length;
+    headers['Content-Length'] = body.length;
 
-    return this._call("PUT", path, host, body, headers);
+    return this._call('PUT', path, host, body, headers);
   }
 
   async delete(path, host, body = {}, headers = {}) {
     body = JSON.stringify(body);
-    headers["Content-Length"] = body.length;
+    headers['Content-Length'] = body.length;
 
-    return this._call("DELETE", path, host, body, headers);
+    return this._call('DELETE', path, host, body, headers);
   }
 
   async _call(type, path, host, body = {}, headers = {}) {
-    const https = require("https");
+    const https = require('https');
 
-    if (!headers["Content-Type"]) {
-      headers["Content-Type"] = "application/json"; // Common situation handled here.
+    if (!headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json'; // Common situation handled here.
     }
 
     const options = {
@@ -57,24 +58,22 @@ module.exports = class HttpsClient {
       const data = [];
 
       const req = https.request(options, res => {
-        let isError = false;
-        if (res.statusCode >= 400) {
-          isError = true;
-        }
+        res.on('data', chunk => data.push(chunk));
+        res.on('error', e => reject(e)); // Network Error
+        res.on('end', () => {
+          const statusCode = res.statusCode;
 
-        res.on("data", chunk => data.push(chunk));
-        res.on("error", e => reject(e)); // Network Error
-        res.on("end", () => {
           try {
             const resStr = Buffer.concat(data).toString();
             let response = resStr;
             try {
               response = JSON.parse(resStr);
+              response.statusCode = statusCode;
             } catch (e) {
               // Everything is fine.
             }
 
-            if (isError) {
+            if (statusCode >= 400) {
               reject(new Error(response)); // Server Error
             } else {
               resolve(response);
@@ -85,7 +84,7 @@ module.exports = class HttpsClient {
         });
       });
 
-      if (type === "POST" || type === "PUT" || type === "DELETE") {
+      if (type === 'POST' || type === 'PUT' || type === 'DELETE') {
         req.write(body);
       }
 
